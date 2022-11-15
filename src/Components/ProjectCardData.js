d808b5bdf15067c2dce7104c84f7b080cb9e318e
= <abhisingh30800@gmail.com>
2022-11-15 18:44:27 +0530

    added interactive--comments-section

--- src/Components/ProjectCardData.js
+++ src/Components/ProjectCardData.js
@@ -1,146 +1,178 @@
 export default [
-    {
-        images: "./images/ecommerce.png",
-        title: "E-commerce Product Landing Page using HTML, CSS & JS",
-        githubUrl : "https://github.com/dazzlerabhi30800/FrontEndMentor-ECommerce-ProductPage",
-        liveUrl: "https://front-end-mentor-ec-ommerce-product-page.vercel.app/",
-        color: "ecommerce",
-        level: "intermediate"
-    },
-    {
-        images: "./images/todoApp.png",
-        title: "Todo App with Local Storage in React",
-        githubUrl : "https://github.com/dazzlerabhi30800/frontiEndMentor-todo-app-with-local-storage",
-        liveUrl: "https://front-end-mentor-todo-app2.vercel.app/",
-        color: "todo",
-        level: "intermediate"
-    },
-    {
-        images: "./images/IPTracker.png",
-        title: "IP Address Tracker in React",
-        githubUrl : "https://github.com/dazzlerabhi30800/frontEndMentor-IP-Address-Tracker",
-        liveUrl: "https://front-end-mentor-ip-address-tracker-1yn6.vercel.app/",
-        color: "tracker",
-        level: "intermediate"
-    },
-    {
-        images: "./images/signUpForm.png",
-        title: "Sign Up form using HTML, CSS & JS",
-        githubUrl : "https://github.com/dazzlerabhi30800/frontEndMentor-IntroComponentWithSign-up-form",
-        liveUrl: "https://front-end-mentor-intro-component-with-sign-up-form.vercel.app/",
-        color: "form",
-        level: "intermediate"
-    },
-    {
-        images: "./images/profileCardComp.png",
-        title: "Profile Card Component using HTML & CSS",
-        githubUrl : "https://github.com/dazzlerabhi30800/FrontEndMentorProjectRetake",
-        liveUrl: "https://front-end-mentor-project-retake.vercel.app/",
-        color: "profile--comp",
-        level: "newbie"
-    },
-    {
-        images: "./images/cardHolder.png",
-        title: "Interactive Card Usingh HTML, CSS, JS & GSAP",
-        githubUrl : "https://github.com/dazzlerabhi30800/FrontEndMentor-InteractiveCardDetails",
-        liveUrl: "https://front-end-mentor-intractive-card-details.vercel.app/",
-        color: "card--comp",
-        level: "junior"
-    },
-    {
-        images: "./images/UrlShortener.png",
-        title: "URL Shortener using HTML, SASS, API in React",
-        githubUrl : "https://github.com/dazzlerabhi30800/FrontEndMentor-url-shortener-API",
-        liveUrl: "https://front-end-mentor-url-shortener-api.vercel.app/",
-        color: "url",
-        level: "intermediate"
-    },
-    {
-        images: "./images/newsHomepage.png",
-        title: "News Homepage using HTML & CSS",
-        githubUrl : "https://github.com/dazzlerabhi30800/frontendMentor-newsHomepage",
-        liveUrl: "https://frontend-mentor-newspage.vercel.app/",
-        color: "news--homepage",
-        level: "junior"
-    },
-    {
-        images: "./images/crowdFundingPage.png",
-        title: "Crowd Funding Page using HTML, CSS & JS",
-        githubUrl : "https://github.com/dazzlerabhi30800/frontEndMentor-CrowdFundingPage",
-        liveUrl: "https://front-end-mentor-crowd-funding-page.vercel.app/",
-        color: "crowdfunding",
-        level: "junior"
-    },
-    {
-        images: "./images/notificationsPage.png",
-        title: "Notifications Page using HTML, SASS & JS",
-        githubUrl : "https://github.com/dazzlerabhi30800/frontendMentor-Notifications-page",
-        liveUrl: "https://frontend-mentor-notifications-page-fczf.vercel.app/",
-        color: "notification",
-        level: "junior"
-    },
-    {
-        images: "./images/fourCardFeature.png",
-        title: "Four Card Feature using HTML & CSS",
-        githubUrl : "https://github.com/dazzlerabhi30800/frontEndMentor-FourCardFeatureSection",
-        liveUrl: "https://front-end-mentor-four-card-feature-section-six.vercel.app/",
-        color: "four--card--feature",
-        level: "newbie"
-    },
-    {
-        images: "./images/clipboard.png",
-        title: "Clipboard Landing Page using HTML & CSS in React",
-        githubUrl : "https://github.com/dazzlerabhi30800/frontEndMentor-clipboardLandingPage",
-        liveUrl: "https://front-end-mentor-clipboard-landing-page-sable.vercel.app/",
-        color: "clipboard",
-        level: "junior"
-    },
-    {
-        images: "./images/socialmediadashboard.png",
-        title: "Social Media DashBoard using HTML & SASS in React",
-        githubUrl : "https://github.com/dazzlerabhi30800/frontEndMentor-SocialMediaDashboard",
-        liveUrl: "https://front-end-mentor-social-media-dashboard-2.vercel.app/",
-        color: "social--dashboard",
-        level: "junior"
-    },
-    {
-        images: "./images/snapIntro.png",
-        title: "Snap Intro Section Page using HTML, CSS & JS",
-        githubUrl : "https://github.com/dazzlerabhi30800/FrontEndMentor-introSection-with-dropdown-navigation",
-        liveUrl: "https://front-end-mentor-intro-section-with-dropdown-navigation.vercel.app/",
-        color: "snap--intro",
-        level: "junior"
-    },
-    {
-        images: "./images/insureLanding.png",
-        title: "Insure Landing Page using HTML, CSS & Tailwind in React",
-        githubUrl : "https://github.com/dazzlerabhi30800/frontEnd-Mentor-insure-landing-Page",
-        liveUrl: "https://front-end-mentor-insure-landing-page-topaz.vercel.app/",
-        color: "insure--landing",
-        level: "junior"
-    },
-    {
-        images: "./images/ratingCard.png",
-        title: "Rating Card using HTML, CSS & JS",
-        githubUrl : "https://github.com/dazzlerabhi30800/FrontEndMentor-interactive-component-main",
-        liveUrl: "https://front-end-mentor-interactive-component-main2.vercel.app/",
-        color: "rating--card",
-        level: "newbie"
-    },
-    {
-        images: "./images/fylo.png",
-        title: "Fylo Data Storage Component using HTML & CSS",
-        githubUrl : "https://github.com/dazzlerabhi30800/FrontEndMentor-Fylo-data-storage-component",
-        liveUrl: "https://front-end-mentor-fylo-data-storage-component-phi.vercel.app/",
-        color: "fylo",
-        level: "junior"
-    },
-    {
-        images: "./images/qrcode.png",
-        title: "QR Code Component using HTML & CSS",
-        githubUrl : "https://github.com/dazzlerabhi30800/frontendMentor-QrCode",
-        liveUrl: "https://frontend-mentor-qr-code-one.vercel.app/",
-        color: "qr--code",
-        level: "newbie"
-    },
-]
+  {
+    images: "./images/ecommerce.png",
+    title: "E-commerce Product Landing Page using HTML, CSS & JS",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/FrontEndMentor-ECommerce-ProductPage",
+    liveUrl: "https://front-end-mentor-ec-ommerce-product-page.vercel.app/",
+    color: "ecommerce",
+    level: "intermediate",
+  },
+  {
+    images: "./images/todoApp.png",
+    title: "Todo App with Local Storage in React",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/frontiEndMentor-todo-app-with-local-storage",
+    liveUrl: "https://front-end-mentor-todo-app2.vercel.app/",
+    color: "todo",
+    level: "intermediate",
+  },
+  {
+    images: "./images/IPTracker.png",
+    title: "IP Address Tracker in React",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/frontEndMentor-IP-Address-Tracker",
+    liveUrl: "https://front-end-mentor-ip-address-tracker-1yn6.vercel.app/",
+    color: "tracker",
+    level: "intermediate",
+  },
+  {
+    images: "./images/signUpForm.png",
+    title: "Sign Up form using HTML, CSS & JS",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/frontEndMentor-IntroComponentWithSign-up-form",
+    liveUrl:
+      "https://front-end-mentor-intro-component-with-sign-up-form.vercel.app/",
+    color: "form",
+    level: "intermediate",
+  },
+  {
+    images: "./images/profileCardComp.png",
+    title: "Profile Card Component using HTML & CSS",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/FrontEndMentorProjectRetake",
+    liveUrl: "https://front-end-mentor-project-retake.vercel.app/",
+    color: "profile--comp",
+    level: "newbie",
+  },
+  {
+    images: "./images/cardHolder.png",
+    title: "Interactive Card Usingh HTML, CSS, JS & GSAP",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/FrontEndMentor-InteractiveCardDetails",
+    liveUrl: "https://front-end-mentor-intractive-card-details.vercel.app/",
+    color: "card--comp",
+    level: "junior",
+  },
+  {
+    images: "./images/UrlShortener.png",
+    title: "URL Shortener using HTML, SASS, API in React",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/FrontEndMentor-url-shortener-API",
+    liveUrl: "https://front-end-mentor-url-shortener-api.vercel.app/",
+    color: "url",
+    level: "intermediate",
+  },
+  {
+    images: "./images/newsHomepage.png",
+    title: "News Homepage using HTML & CSS",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/frontendMentor-newsHomepage",
+    liveUrl: "https://frontend-mentor-newspage.vercel.app/",
+    color: "news--homepage",
+    level: "junior",
+  },
+  {
+    images: "./images/crowdFundingPage.png",
+    title: "Crowd Funding Page using HTML, CSS & JS",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/frontEndMentor-CrowdFundingPage",
+    liveUrl: "https://front-end-mentor-crowd-funding-page.vercel.app/",
+    color: "crowdfunding",
+    level: "junior",
+  },
+  {
+    images: "./images/notificationsPage.png",
+    title: "Notifications Page using HTML, SASS & JS",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/frontendMentor-Notifications-page",
+    liveUrl: "https://frontend-mentor-notifications-page-fczf.vercel.app/",
+    color: "notification",
+    level: "junior",
+  },
+  {
+    images: "./images/fourCardFeature.png",
+    title: "Four Card Feature using HTML & CSS",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/frontEndMentor-FourCardFeatureSection",
+    liveUrl:
+      "https://front-end-mentor-four-card-feature-section-six.vercel.app/",
+    color: "four--card--feature",
+    level: "newbie",
+  },
+  {
+    images: "./images/clipboard.png",
+    title: "Clipboard Landing Page using HTML & CSS in React",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/frontEndMentor-clipboardLandingPage",
+    liveUrl:
+      "https://front-end-mentor-clipboard-landing-page-sable.vercel.app/",
+    color: "clipboard",
+    level: "junior",
+  },
+  {
+    images: "./images/socialmediadashboard.png",
+    title: "Social Media DashBoard using HTML & SASS in React",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/frontEndMentor-SocialMediaDashboard",
+    liveUrl: "https://front-end-mentor-social-media-dashboard-2.vercel.app/",
+    color: "social--dashboard",
+    level: "junior",
+  },
+  {
+    images: "./images/snapIntro.png",
+    title: "Snap Intro Section Page using HTML, CSS & JS",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/FrontEndMentor-introSection-with-dropdown-navigation",
+    liveUrl:
+      "https://front-end-mentor-intro-section-with-dropdown-navigation.vercel.app/",
+    color: "snap--intro",
+    level: "junior",
+  },
+  {
+    images: "./images/insureLanding.png",
+    title: "Insure Landing Page using HTML, CSS & Tailwind in React",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/frontEnd-Mentor-insure-landing-Page",
+    liveUrl: "https://front-end-mentor-insure-landing-page-topaz.vercel.app/",
+    color: "insure--landing",
+    level: "junior",
+  },
+  {
+    images: "./images/ratingCard.png",
+    title: "Rating Card using HTML, CSS & JS",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/FrontEndMentor-interactive-component-main",
+    liveUrl: "https://front-end-mentor-interactive-component-main2.vercel.app/",
+    color: "rating--card",
+    level: "newbie",
+  },
+  {
+    images: "./images/fylo.png",
+    title: "Fylo Data Storage Component using HTML & CSS",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/FrontEndMentor-Fylo-data-storage-component",
+    liveUrl:
+      "https://front-end-mentor-fylo-data-storage-component-phi.vercel.app/",
+    color: "fylo",
+    level: "junior",
+  },
+  {
+    images: "./images/qrcode.png",
+    title: "QR Code Component using HTML & CSS",
+    githubUrl: "https://github.com/dazzlerabhi30800/frontendMentor-QrCode",
+    liveUrl: "https://frontend-mentor-qr-code-one.vercel.app/",
+    color: "qr--code",
+    level: "newbie",
+  },
+  {
+    images: "./images/interactive-comments-section.png",
+    title: "Interactive Comments Section HTML, CSS & JS",
+    githubUrl:
+      "https://github.com/dazzlerabhi30800/frontendMentor-interactive-comments-section-retake",
+    liveUrl:
+      "https://frontend-mentor-interactive-comments-section-retake.vercel.app/",
+    color: "interactive--comments",
+    level: "intermediate",
+  },
+];
