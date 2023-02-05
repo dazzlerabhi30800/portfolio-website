0b3eb46241ac396bf2cb91265be3af2f639194d3
= <abhisingh30800@gmail.com>
2023-02-05 11:32:37 +0530

    added new skills component

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,13 +1,44 @@
 import React from "react";
-import ProgressCircle from "./ProgressCirlce";
 
 const Skills = ({ skillsContainer }) => {
+  const logoImg = [
+    "./logo/nodejs.png",
+    "./logo/css.png",
+    "./logo/sass.png",
+    "./logo/bootstrap.png",
+    "./logo/figma.png",
+    "./logo/javascript.png",
+    "./logo/react.png",
+    "./logo/github.png",
+    "./logo/tailwind.png",
+    "./logo/vite.png",
+  ];
+  window.addEventListener("scroll", () => {
+    const slideAt =
+      window.scrollY +
+      window.innerHeight -
+      skillsContainer.current.clientHeight / 2;
+    // bottom of the image
+    const imageBottom =
+      skillsContainer.current.offsetTop + skillsContainer.current.clientHeight;
+    const isHalfShown = slideAt > skillsContainer.current.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      skillsContainer.current.classList.add("show");
+    } else {
+      skillsContainer.current.classList.remove("show");
+    }
+  });
   return (
     <section className="skills--wrapper" id="skills">
       <div className="title">
-        <h1>SkILLS</h1>
+        <h1>My Arsenal for Coding</h1>
       </div>
-      <ProgressCircle skillsContainer={skillsContainer} />
+      <div className="skills--container" ref={skillsContainer}>
+        {logoImg.map((img, i) => {
+          return <img src={img} key={i} alt="" />;
+        })}
+      </div>
     </section>
   );
 };
